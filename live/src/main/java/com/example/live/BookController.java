c1010261e5adb390a0c00a8f9e24652823589f11
Egor Samoylov <mr.egoras@mail.ru>
2025-02-06 15:45:09 +0300

    Внутри контроллера BookController написал метод Post для создания новой книги

--- live/src/main/java/com/example/live/BookController.java
+++ live/src/main/java/com/example/live/BookController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +38,11 @@ public class BookController {
         Optional<Book> book = bookRepository.findById(id);
         return book.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
             .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book createBook = bookRepository.save(book);
+        return new ResponseEntity<>(createBook, HttpStatus.CREATED);
     }
 }
