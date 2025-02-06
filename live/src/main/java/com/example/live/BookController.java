ee46b5f1ddbf50a17311cf9dcb08694b9773c32f
Egor Samoylov <mr.egoras@mail.ru>
2025-02-06 16:00:24 +0300

    Внутри контроллера BookController создание метода Put для изменения книги Плюс небольшое изменение в файле зависимостей pom.xml (закомментирование строки с тегом <scope>)

--- live/src/main/java/com/example/live/BookController.java
+++ live/src/main/java/com/example/live/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,5 +45,17 @@ public class BookController {
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
         Book createBook = bookRepository.save(book);
         return new ResponseEntity<>(createBook, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}") 
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Optional<Book> existingBook = bookRepository.findById(id);
+       if (existingBook.isPresent()) {
+           book.setId(id);
+           Book updatedBook = bookRepository.save(book);
+           return new ResponseEntity<>(updatedBook, HttpStatus.OK);
+       } else {
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+       }
     }
 }
