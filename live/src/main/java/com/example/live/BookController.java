b1bda02a8129dab3984934a882541cb486925103
Egor Samoylov <mr.egoras@mail.ru>
2025-02-09 20:09:29 +0300

    В application.properties добавили password для нашего username. В BookController добавили CrossOrogin (настройка CORS) для испольозвания любого порта localhost:*.

--- live/src/main/java/com/example/live/BookController.java
+++ live/src/main/java/com/example/live/BookController.java
@@ -1,12 +1,12 @@
 package com.example.live;
 
-import java.lang.classfile.ClassFile;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //для преобразования ответов в JSON или XML (RESTful веб сервис)
+@CrossOrigin(origins = "http://localhost:*")
 @RequestMapping("/books")
 public class BookController {
     @Autowired  // для автоматического связывания компонентов бина между собой
